0c65a0d19cedbd6e5e69216b87394621eaa0ead2
dhruvoneknotone <160725997+dhruvoneknotone@users.noreply.github.com>
2024-09-09 14:47:26 +0530

    Update online.ps1

--- online.ps1
+++ online.ps1
@@ -21,18 +21,21 @@ $BASE_PATH = "C:/Track"
 
 # Create the base path if it does not exist
 if (-not (Test-Path $BASE_PATH)) {
-    New-Item -ItemType Directory -Path $BASE_PATH
+    New-Item -ItemType Directory -Path $BASE_PATH -Force
 }
 
 # Clone the GitHub repository into the base path
-git clone https://github.com/dhruvoneknotone/Track.git "$BASE_PATH"
+git clone https://github.com/dhruvoneknotone/Track.git "$BASE_PATH" -q
 
 Write-Host "Repository cloned successfully."
 
-# Create a batch file to start main.bat in the base path
-$BAT_FILE = "$BASE_PATH\start_main.bat"
-Set-Content -Path $BAT_FILE -Value "cd $BASE_PATH`nmain.bat"
-
-# Move the batch file to the startup folder
+# Check if any file exists in the startup folder
 $STARTUP_FOLDER = "C:\Users\DeLL\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-Move-Item -Path $BAT_FILE -Destination $STARTUP_FOLDER
+if (-not (Get-ChildItem -Path $STARTUP_FOLDER -ErrorAction SilentlyContinue)) {
+    # Create a batch file to start main.bat in the base path
+    $BAT_FILE = "$BASE_PATH\start_main.bat"
+    Set-Content -Path $BAT_FILE -Value "cd $BASE_PATH`nmain.bat" -Force
+
+    # Move the batch file to the startup folder
+    Move-Item -Path $BAT_FILE -Destination $STARTUP_FOLDER -Force
+}
