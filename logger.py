60e9aac9f5610325f7747a0004b4dcd75c3f9bc7
dhruvoneknotone <160725997+dhruvoneknotone@users.noreply.github.com>
2024-09-24 16:48:34 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,142 +1,122 @@
+# logger.py
+"""
+Module for system monitoring and logging.
+"""
+
 import os
 import time
 from datetime import datetime, timedelta
-import psutil
-import threading
 import sys
 import logging
 import socket
+import threading
+import psutil
 
 def resource_path(relative_path):
     """
-    This function returns the absolute path of a resource file.
-    It checks if the script is running in a frozen environment (e.g., PyInstaller) and returns the path accordingly.
+    Returns the absolute path to a resource file.
     """
     try:
-        base_path = sys._MEIPASS  # Check if the script is running in a frozen environment
-    except Exception:
-        base_path = os.path.abspath(
-            os.path.dirname(__file__)
-        )  # If not frozen, use the directory of the script
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(os.path.dirname(__file__))
     return os.path.join(base_path, relative_path)
 
 def setup_logging():
     """
-    This function sets up the logging configuration.
-    It creates a log directory with the current date and sets up a basic configuration for logging.
+    Sets up the logging configuration.
     """
-    current_date = datetime.now().strftime('%d-%m-%Y')  # Get the current date
-    log_dir = os.path.join(
-        resource_path("Logs"), current_date
-    )
-    os.makedirs(log_dir, exist_ok=True)  # Ensure the directory exists
+    current_date = datetime.now().strftime('%d-%m-%Y')
+    log_dir = os.path.join(resource_path("Logs"), current_date)
+    os.makedirs(log_dir, exist_ok=True)
     log_file = os.path.join(
         log_dir, 
         f"system_monitor_{datetime.now().strftime('%d-%m-%Y_%H-%M')}.txt"
     )
     logging.basicConfig(
-        filename=log_file, level=logging.INFO,
+        filename=log_file, 
+        level=logging.INFO, 
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
 def log_system_startup():
     """
-    This function logs the system startup information.
-    It logs the computer name and system startup time.
+    Logs system startup information.
     """
-    computer_name = socket.gethostname()  # Get the computer name
-    logging.info(f"Computer Name: {computer_name}")
+    computer_name = socket.gethostname()
+    logging.info("Computer Name: %s", computer_name)
     print(f"Computer Name: {computer_name}")
-    boot_time = datetime.fromtimestamp(
-        psutil.boot_time()
-    ).strftime("%d-%m-%Y")  # Get the system startup time
-    logging.info(f"System Startup: {boot_time}")
+    boot_time = datetime.fromtimestamp(psutil.boot_time()).strftime("%d-%m-%Y")
+    logging.info("System Startup: %s", boot_time)
     print(f"System Startup: {boot_time}")
 
 def monitor_system_processes():
     """
-    This function monitors system processes.
-    It logs processes that have started or restarted within the last 10 minutes.
+    Monitors system processes for a specified duration.
     """
     last_logged_processes = {}
     start_time = datetime.now()
     while datetime.now() - start_time < timedelta(hours=1):
         current_time = datetime.now().strftime("%d-%m-%Y_%H-%M")
-        logging.info(f"Process Check: {current_time}")
+        logging.info("Process Check: %s", current_time)
         for proc in psutil.process_iter(['pid', 'exe']):
             try:
                 info = proc.info
                 process_key = f"{info['exe']}"
-                if process_key not in last_logged_processes or \
-                   datetime.now() - last_logged_processes[process_key] > timedelta(minutes=10):
-                    logging.info(f"Process: {info['exe']}")
+                if process_key not in last_logged_processes or datetime.now() - last_logged_processes[process_key] > timedelta(minutes=10):
+                    logging.info("Process: %s", info['exe'])
                     last_logged_processes[process_key] = datetime.now()
-            except (psutil.NoSuchProcess, 
-                    psutil.AccessDenied, 
-                    psutil.ZombieProcess):
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
         time.sleep(60)
 
 def monitor_system_file_operations():
     """
-    This function monitors system file operations.
-    It logs file operations that have occurred within the last 10 minutes.
+    Monitors system file operations for a specified duration.
     """
     last_logged_file_operations = {}
     start_time = datetime.now()
     while datetime.now() - start_time < timedelta(hours=1):
         current_time = datetime.now().strftime("%d-%m-%Y_%H-%M")
-        logging.info(f"File Operation Check: {current_time}")
+        logging.info("File Operation Check: %s", current_time)
         for proc in psutil.process_iter(['pid', 'exe']):
             try:
                 info = proc.info
                 if info['exe']:
                     file_operation_key = f"{info['exe']}"
-                    if file_operation_key not in last_logged_file_operations or \
-                       datetime.now() - last_logged_file_operations[file_operation_key] > timedelta(minutes=10):
-                        logging.info(f"File Operation: {info['exe']}")
+                    if file_operation_key not in last_logged_file_operations or datetime.now() - last_logged_file_operations[file_operation_key] > timedelta(minutes=10):
+                        logging.info("File Operation: %s", info['exe'])
                         last_logged_file_operations[file_operation_key] = datetime.now()
-            except (psutil.NoSuchProcess, 
-                    psutil.AccessDenied, 
-                    psutil.ZombieProcess):
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
         time.sleep(60)
 
 def main():
     """
-    This is the main function of the script.
-    It sets up logging, logs system startup, and starts threads for process and file operation monitoring.
+    Main function to execute the system monitoring tasks.
     """
     current_date = datetime.now().strftime('%d-%m-%Y')
-    log_dir = os.path.join(
-        resource_path("Logs"), current_date
-    )
-    os.makedirs(log_dir, exist_ok=True)  # Ensure the directory exists
+    log_dir = os.path.join(resource_path("Logs"), current_date)
+    os.makedirs(log_dir, exist_ok=True)
     txt_files = [f for f in os.listdir(log_dir) if f.endswith('.txt')]
-    print("Text files found:", txt_files)  # Debug print
-    if txt_files :
+    print("Text files found:", txt_files)
+    if txt_files:
         print("A txt file already exists in the path. Exiting the program.")
-    else:
-        try:
-            setup_logging()
-            log_system_startup()
+        return 1  # Ensure consistent return
 
-            process_thread = threading.Thread(
-                target=monitor_system_processes
-            )
-            file_operation_thread = threading.Thread(
-                target=monitor_system_file_operations
-            )
+    setup_logging()
+    log_system_startup()
 
-            process_thread.start()
-            file_operation_thread.start()
+    process_thread = threading.Thread(target=monitor_system_processes)
+    file_operation_thread = threading.Thread(target=monitor_system_file_operations)
 
-            process_thread.join()
-            file_operation_thread.join()
-        except Exception as e:
-            logging.error(f"An error occurred: {str(e)}")
-            return 1
-        return 0
+    process_thread.start()
+    file_operation_thread.start()
+
+    process_thread.join()
+    file_operation_thread.join()
+
+    return 0
 
 if __name__ == "__main__":
-    main()  # Call the main function to start the program
+    main()
